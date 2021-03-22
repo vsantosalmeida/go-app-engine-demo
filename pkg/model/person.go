011600a2220864644eb62255caf715295dd011df
valmeida75 <valmeida@uolinc.com>
2021-03-21 22:51:37 -0300

    Alteração na datastore.Key

--- pkg/model/person.go
+++ pkg/model/person.go
@@ -2,31 +2,16 @@ package model
 
 import (
 	"cloud.google.com/go/datastore"
-	"strings"
 )
 
 type Person struct {
-	Key       string  `json:"key,omitempty"`
-	Firstname string  `json:"firstname,omitempty"`
-	Lastname  string  `json:"lastname,omitempty"`
-	Address   Address `json:"address,omitempty" datastore:",flatten"`
+	Key       *datastore.Key `json:"key,omitempty" datastore:"__key__"`
+	Firstname string         `json:"firstname,omitempty"`
+	Lastname  string         `json:"lastname,omitempty"`
+	Address   Address        `json:"address,omitempty" datastore:",flatten"`
 }
 
 type Address struct {
 	City  string `json:"city,omitempty"`
 	State string `json:"state,omitempty"`
-}
-
-func (p *Person) LoadKey(k *datastore.Key) error {
-	splitKey := strings.Split(k.String(), ",")
-	p.Key = splitKey[1]
-	return nil
-}
-
-func (p *Person) Load(ps []datastore.Property) error {
-	return datastore.LoadStruct(p, ps)
-}
-
-func (p *Person) Save() ([]datastore.Property, error) {
-	return datastore.SaveStruct(p)
 }
